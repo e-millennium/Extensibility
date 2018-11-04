a4b263482328061996bff99755e774c3f27bdaaf
Flávio Oliveira <flavio.oliveira@millennium.com.br>
2018-11-03 22:57:07 -0300

    vÁRTIOS AJUSTES

--- millennium!terminal/views-win32/TerminalConsultaPreco.dpr
+++ millennium!terminal/views-win32/TerminalConsultaPreco.dpr
@@ -4,7 +4,8 @@ uses
   Forms,
   Principal in 'Principal.pas' {FTerminal},
   Background in 'views-win32\Background.pas',
-  ListaProdutos in 'ListaProdutos.pas' {FListaProdutos};
+  ListaProdutos in 'ListaProdutos.pas' {FListaProdutos},
+  Observacao in 'Observacao.pas' {FSaibaMais};
 
 {$R *.RES}
 
@@ -13,5 +14,6 @@ begin
   Application.Title := 'Terminal de Consulta de Produtos';
   Application.CreateForm(TFTerminal, FTerminal);
   Application.CreateForm(TFListaProdutos, FListaProdutos);
+  Application.CreateForm(TFSaibaMais, FSaibaMais);
   Application.Run;
 end.
